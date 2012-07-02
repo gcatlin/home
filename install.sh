376972f6e2f59213e77e219c43c05f16eca8302c
Geoff Catlin <gcatlin@gmail.com>
2012-07-02 13:34:58 -0400

    Adding install.sh and instructions

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+function command_exists () { hash "$1" 2>&- ; }
+
+if command_exists git ; then
+	git clone https://github.com/gcatlin/home.git ~/.home &&
+	~/.home/bin/dfm &&
+	mkdir -p ~/.vim/bundle/vundle &&
+	git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle &&
+	vim -c ':BundleInstall' -c ':qa!'
+else
+	mkdir ~/.home &&
+	curl -skL https://github.com/gcatlin/home/tarball/master | tar -zx --strip 1 -C ~/.home &&
+	~/.home/bin/dfm &&
+	mkdir -p ~/.vim/bundle/vundle &&
+	curl -skL https://github.com/gmarik/vundle/tarball/master | tar -zx --strip 1 -C ~/.vim/bundle/vundle &&
+	vim -c ':BundleInstall' -c ':qa!'
+fi
+
+. ~/.bashrc
+
+
