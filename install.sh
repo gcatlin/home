1912691bcebb8e5506fd7f5911cfc97ed1765c9d
Geoff Catlin <gcatlin@gmail.com>
2013-02-07 22:03:22 -0500

    Added creation of ~/.vim/tmp dir in install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ function command_exists () { hash "$1" 2>&- ; }
 if command_exists git ; then
 	git clone https://github.com/gcatlin/home.git ~/.home &&
 	~/.home/bin/dfm &&
+	mkdir -p ~/.vim/tmp &&
 	mkdir -p ~/.vim/bundle/vundle &&
 	git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle &&
 	vim -c ':BundleInstall' -c ':qa!'
@@ -12,6 +13,7 @@ else
 	mkdir ~/.home &&
 	curl -skL https://github.com/gcatlin/home/tarball/master | tar -zx --strip 1 -C ~/.home &&
 	~/.home/bin/dfm &&
+	mkdir -p ~/.vim/tmp &&
 	mkdir -p ~/.vim/bundle/vundle &&
 	curl -skL https://github.com/gmarik/vundle/tarball/master | tar -zx --strip 1 -C ~/.vim/bundle/vundle &&
 	vim -c ':BundleInstall' -c ':qa!'
